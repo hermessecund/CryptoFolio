8ffac9b431a34885231c07fff08c27c5f014515a
Tarwar <69816954+hermessecund@users.noreply.github.com>
2022-09-24 22:51:13 +0100

    Update NavBar.js

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,13 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
-import Diamond from "./logocrypto.jpg";
+import Diamond from "./v2.png";
 
 const NavBar = (props) => {
   return (
     <nav className="shadow-[0_3px_8px_rgba(0,0,0,0.24)] h-16 px-8 flex items-center justify-between">
       <div className="flex items-center">
         <img className="w-10 h-10" alt="logo" src={Diamond}/>
-        <h1 className="text-lg font-bold ml-4">allcoins</h1>
+        <h1 className="text-lg font-bold ml-4">TiFi</h1>
       </div>
       <div>
         {props.themeStatus && (
